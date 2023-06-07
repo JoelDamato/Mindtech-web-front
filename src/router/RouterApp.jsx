4d03696babc38983c9c39eb1fab953211155eef6
JoelDamato <127883956+JoelDamato@users.noreply.github.com>
2023-06-07 16:10:55 -0300

    J register/login (#3)
    * Update README.md
    
    * creating page login (sing in/ sing up)
    
    ---------
    
    Co-authored-by: Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import App from "../App";
 import Demostration from "../pages/Demostration";
 import Index from "../pages/Index";
+import Login from "../pages/login";
 
 export default function RouterApp() {
   return (
     <Routes>
       <Route element={<App />}>
         <Route path="/" element={<Index />} />
+       
       </Route>
+      <Route path="/register/login" element= {<Login/>} />
       <Route path="/demostration" element={<Demostration />} />
     </Routes>
   );
