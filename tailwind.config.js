aa905955892fdc3a6a8397b97ba97073f7fe52b6
joacodimaro97 <joacodimaro97@gmail.com>
2023-06-07 15:47:51 -0300

    Joaquindimaro index (#2)
    * Update README.md
    
    * diseño-navbar
    
    * index full responsive
    
    ---------
    
    Co-authored-by: Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens:{
+        'mob': {'min': '320px', 'max': '639px'}
+      },
       fontFamily: {
         'montserrat': ['Montserrat', 'sans-serif'],
       },
