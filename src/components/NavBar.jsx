07d456675eaa17b55b60f0b7f97e5aec00adeaf7
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-06-08 14:35:29 -0300

    Store/index

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="fixed bg-[#000000e2] text-white mx-auto px-5 py-3 flex  sm:justify-between items-center justify-between w-full h-[10vh]  font-montserrat">
+    <div className="fixed z-10 bg-[#000000e2] text-white mx-auto px-5 py-3 flex  sm:justify-between items-center justify-between w-full h-[10vh]  font-montserrat">
       <a className=" mob:mt-6 text-[24px] w-[278px] lg:w-[30%] h-[8vh] justify-center  flex flex-col items-center hover:text-[#cacaca] transition-colors cursor-pointer tracking-[20px] sm:tracking-[10px] font-semibold mob:text-[14px] mob:tracking-[10px] mob:w-[160px]  sm:text-[14px]  sm:w-[160px]   ">
-        <p className="lg:text-[24px] lg:tracking-[30px]" >MINDTECH</p>
+        <p className="lg:text-[24px] lg:tracking-[20px] xl:tracking-[30px]" >MINDTECH</p>
         <p className="text-[16px] sm:text-[8px] font-light lg:tracking-[20px] mob:text-[6px]">TECHNO</p>
       </a>
 
@@ -44,13 +44,13 @@ export default function NavBar() {
       <div className={`  sm:w-[80%] sm:flex flex-grow ${menuOpen ? "block" : "hidden"} mob:absolute mob:right-[140px] mob:top-[20px] sm:h-[8vh] sm:items-center  lg:w-[70%]  `}>
         <ul className="flex  flex-col sm:flex-row  sm:w-[100%] sm:justify-evenly mob:absolute mob:bg-[#000000e0] mob:rounded-[2%] mob:p-1 ">
           <li className="text-lg font-medium sm:mr-4 sm:text-[11px] md:text-[16px] hover:border-b-[1px]  hover:border-white ">
-            <Link to="">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li className="text-lg font-medium sm:mr-4 sm:text-[11px] md:text-[16px] hover:border-b-[1px] hover:border-white">
             <Link to="">Login | Register</Link>
           </li>
           <li className="text-lg font-medium sm:mr-4 sm:text-[11px] md:text-[16px] hover:border-b-[1px] hover:border-white">
-            <Link to="">Store</Link>
+            <Link to="/store">Store</Link>
           </li>
           <li className="text-lg font-medium sm:text-[11px] md:text-[16px] hover:border-b-[1px] hover:border-white">
             <Link to="">Details</Link>
