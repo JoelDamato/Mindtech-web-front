8004906362b9c2526e2332fe49a4a839c3d98000
joacodimaro97 <joacodimaro97@gmail.com>
2023-06-09 13:25:10 -0300

    Joaquindimaro index (#7)
    * Update README.md
    
    * diseño-navbar
    
    * index full responsive
    
    * Store/index
    
    * store map
    
    ---------
    
    Co-authored-by: Joaquin Meneses <69328865+JoaquinMeneses@users.noreply.github.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,22 +4,22 @@ export default function Card() {
 
   const  dataPc = [
     {
-        name : 'PC GAMER (?) AHRE',
+        name : 'PC GAMER ',
         description : 'Una computadora personal (PC) es un dispositivo electrónico diseñado para procesar, almacenar y manejar información de manera rápida y eficiente. Combina componentes como la CPU, memoria, almacenamiento y periféricos',
         price : 15000,
-        img : 'https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        img : 'https://i.postimg.cc/DZBVdQxm/card-removebg-preview-16.png'
     },
     {
         name : 'TECLADO GAMER',
         description : 'Un teclado gamer personal electrónico diseñado para jugar hasta altas horas de la noche',
         price : 25000,
-        img : 'https://images.pexels.com/photos/841228/pexels-photo-841228.jpeg?auto=compress&cs=tinysrgb&w=600'
+        img : 'https://i.postimg.cc/vmXM3dBQ/card-removebg-preview-19.png'
     },
     {
         name : 'AUDIFONOS',
         description : 'Los audífonos son dispositivos de audio portátiles que se colocan sobre o dentro de los oídos para escuchar sonidos y música de forma personal. Están compuestos por altavoces o transductores que convierten las señales eléctricas en ondas sonoras audibles para el usuario',
         price : 45000,
-        img : 'https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        img : 'https://i.postimg.cc/SQ16HX59/card-removebg-preview-21.png'
     }
 ]
 
@@ -28,8 +28,8 @@ export default function Card() {
 
       <>
       {dataPc.map((item, index) => (<div className=" text-black  w-[90%] h-[90%] m-2  border border-black rounded-[23px] flex flex-col sm:w-[40%] sm:h-[80%] md:w-[35%] md:h-[95%] lg:w-[30%] lg:h-[100%] ">
-       <div className='border  h-[60%] rounded-t-[23px] flex justify-center p-2 '>
-        <img src={item.img} alt="" className='w-[90%] h-[98%] object-contain ' />
+       <div className='border  h-[60%] rounded-t-[23px] flex flex-col justify-center items-center p-2 '>
+        <img src={item.img} alt="" className='w-[80%]  h-[98%] object-contain ' />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import App from "../App";
+import AdminPanel from "../pages/AdminPanel";
 import Demostration from "../pages/Demostration";
 import Index from "../pages/Index";
 import Login from "../pages/login";
 import Details from "../pages/details";
 import Store from "../pages/Store";
+import Admin from "../pages/AdminPanel";
 
 export default function RouterApp() {
   return (
@@ -16,6 +18,7 @@ export default function RouterApp() {
       <Route path="/register/login" element= {<Login/>} />
       <Route path="/demostration" element={<Demostration />} />
       <Route path="/store" element= {<Store/>} />
+      <Route path="/admin" element= {<Admin/>} />
     </Routes>
   );
 }
